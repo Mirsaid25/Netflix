d22ceb659c6bcaed0f604c9b71b30d47cbb9d27c
Mirsaid25 <siddikovmirsaid01@gmail.com>
2023-03-16 17:22:53 +0500

    16.03

--- src/pages/filmcard/[indexid].tsx
+++ src/pages/filmcard/[indexid].tsx
@@ -4,10 +4,11 @@ import { useRouter } from 'next/router'
 import { APIkey } from '..'
 import axios from 'axios'
 import Link from 'next/link'
-import { BsChevronRight } from 'react-icons/bs';
+import { BsArrowRight, BsChevronRight } from 'react-icons/bs';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
 import Image from 'next/image'
+import { AiFillDislike, AiFillLike } from 'react-icons/ai'
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 type moveInfoT = {
@@ -56,6 +57,7 @@ export const data = {
 function index() {	
 	const [moveInfo, setMoveInfo] = useState<moveInfoT>()
 	const [moveCrew, setMoveCrew] = useState<moveCrewT>()
+	const [tralerKey , setTralerKey] = useState("")
 
 
 	function GetId() {
@@ -82,9 +84,18 @@ function index() {
 				
 			}
 		})
+
+		axios.get(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=${APIkey}&language=en-US`)
+		    .then(res => res.data.results.filter((item: any)=> {
+				if (item.type === "Trailer") {
+					setTralerKey(item.key)
+				}
+			}))
+			
+
 	}, [])
 
-	console.log(moveCrew);
+	console.log(moveInfo);
 		
 	// function movePeople(name:string) {
 	// 	if(moveCrew !== undefined){
@@ -98,7 +109,7 @@ function index() {
 
   return (
     <AppLayout>
-        <section className={`px-[100px] relative`}>
+        <section className={`px-[100px] relative mb-10`}>
 			{/* <img src={`https://www.themoviedb.org/t/p/w220_and_h330_face/${moveInfo?.backdrop_path}`} className="absolute w-full top-0 left-0 -z-[1]" alt="" /> */}
 			<div className='flex justify-between gap-10 mb-[45px]'>
 				<div>
@@ -154,19 +165,32 @@ function index() {
               		  <td className='text-lg font-medium'>Ganre</td>
               		  <td className='text-[#F2F60F] text-lg'>{moveInfo?.genres.at(0)?.name}</td>
               		</tr>
-					  <tr>
-              		  <td className='text-lg font-medium'>Director</td>
-              		  <td className='text-[#F2F60F] text-lg'>{}</td>
-              		</tr>
-                </table>
-				<table className='w-1/2'>
-              		<tr>
-              		  <td className='text-lg font-medium'>Country</td>
-              		  <td className='text-[#F2F60F] text-lg'></td>
-              		</tr>
                 </table>
 			</div>
 		</section>
+		<section className="section2 mb-[75px]">
+				<div className="flex items-center justify-between mb-[40px] max-sm:flex-col max-sm:gap-2 max-sm:mb-2">
+					<h1 className="text-white font-black text-[65px] max-[1580px]:text-[50px] max-xl:text-[40px] max-sm:text-[32px]">Трейлер фильма</h1>
+					<div className="flex items-center gap-5 cursor-pointer">
+						<p className="text-white text-[22px] max-xl:text-[18px] font-bold">Все трейлеры</p>
+						<BsArrowRight color="white" size={26}/>
+					</div>
+				</div>
+				<div className="mb-[50px] max-lg:mb-[30px] max-md:mb-[25px] max-sm:mb-[20px]">
+					<iframe title="trailer" src={`https://www.youtube.com/embed/${tralerKey}`} frameBorder="0" className="w-full h-[800px] rounded-xl mb-5 max-xl:h-[511px] max-lg:h-[450px] max-md:h-[370px] max-sm:h-[250px] max-[425px]:h-[200px]"></iframe>
+					<div className="flex items-center justify-between">
+                        <h1 className="text-white text-[45px] max-lg:text-[35px] max-md:text-[30px] max-sm:text-[25px] font-black">{moveInfo !== undefined ? moveInfo.original_title : null}</h1>
+						<div className="flex items-center gap-2">
+							<div className="bg-[#1B2133] w-[58px] h-[58px] max-lg:h-[40px] max-lg:w-[40px] max-md:h-[30px] max-md:w-[30px] rounded-xl flex items-center justify-center cursor-pointer">
+								<AiFillLike color="white" size={26} className="max-lg:w-[17px] max-lg:h-[17px] max-md:w-[13px] max-md:h-[13px] "/>
+							</div>
+							<div className="bg-[#1B2133] w-[58px] h-[58px] max-lg:h-[40px] max-lg:w-[40px] max-md:h-[30px] max-md:w-[30px] rounded-xl flex items-center justify-center cursor-pointer">
+								<AiFillDislike color="white" size={26} className="max-lg:w-[17px] max-lg:h-[17px] max-md:w-[13px] max-md:h-[13px] "/>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
     </AppLayout>
   )
 }
