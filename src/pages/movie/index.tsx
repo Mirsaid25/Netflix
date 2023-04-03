64282c878cb75001a79823abf576aded1c409400
Mirsaid25 <siddikovmirsaid01@gmail.com>
2023-04-03 18:13:30 +0500

    03.04

--- src/pages/movie/index.tsx
+++ src/pages/movie/index.tsx
@@ -1,3 +1,4 @@
+import SectionOneItem from '@/Components/index_child/sectionOneItem'
 import AppLayout from '@/Layouts/AppLayout'
 import axios from 'axios'
 import { elements } from 'chart.js'
@@ -8,25 +9,62 @@ import { BsChevronRight } from 'react-icons/bs'
 import { APIkey } from '..'
 
 const index =()=> {
-	const [ganreArr , setGanreArr] = useState([])
-	const [ganreActive , setGanreActive] = useState([{}])
+	const [arr , setArr] = useState([])
+	const [filteredArr , setFilteredArr] = useState([])
+
+	const [ganreArr , setGanreArr] = useState<any>([])
+	const [ganreActive , setGanreActive] = useState([{id:0}])
+
 	const [filterHandle , setFilterHandle] = useState(false)
 
 
     useEffect(() => {
+        axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${APIkey}&language=en-US&page=1`)
+	      .then(res1 => {
+			if(res1.status === 200 || res1.status === 201){
+			    setArr(res1.data.results)
+				setFilteredArr(res1.data.results)
+		    } 
+		})
+
+		// axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${APIkey}&language=en-US&page=2`)
+	    //   .then(res => res.status === 200 || res.status === 201 ? setArr(...arr , ...res.data.results): null)
+
 	    axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${APIkey}&language=en-US`)
 		    .then(res=>res.status === 200 || res.status === 201 ? setGanreArr(res.data.genres) : null)
 	}, [])
 	
-	console.log(ganreActive);
+	useEffect(() => {
+		setFilteredArr(  
+		    arr.filter((item: any)=>{
+			    for(let ganre of ganreActive){
+					for( let item2 of item.genre_ids){
+			    		if(item2 === ganre.id){
+							return item
+							// console.log(item);
+							
+			    		}
+			    	}
+			    }
+				// return item
+		    })
+		)
+	}, [ganreActive])
 	
-	function filtering(element: EventTarget){
-	// 	// console.log(element.checked);
-	// 	if(element.checked){
-	// 		setGanreActive([...ganreActive ,{id: element.title}])
-	// 		// console.log(element);
-			
-	// 	}
+	function filtering(element: any){
+		
+
+		if(element.checked){
+			setGanreActive([...ganreActive ,{id: +element.title}])
+		} else{
+		    setGanreActive(
+				ganreActive.filter(item=> {
+				    if (item.id !== +element.title) {
+				        return item
+				    }
+				})	
+			)
+		}
 	
 	}
 
@@ -50,6 +88,10 @@ const index =()=> {
 							<img src="/icons/filter_icon.svg" alt="" className={filterHandle ? 'w-[11px] h-[11px]' :'w-[11px] h-[11px] rotate-180' } />
 						</div>
 						<div className='flex flex-col gap-3'>
+						        <div className='flex items-center gap-2'>
+							        <input onClick={(e) => filtering(e.target)} type="checkbox" name={"all"} title={"all"} id="" className='w-4 h-4 cursor-pointer'/>
+                                    <p className='text-white font-medium'>All</p>
+							    </div>
 							{ganreArr.length >0 ?  ganreArr?.map((item:any) =>(
 								<div className='flex items-center gap-2'>
 							        <input onClick={(e) => filtering(e.target)} type="checkbox" name={item.name} title={item.id} id="" className='w-4 h-4 cursor-pointer'/>
@@ -61,6 +103,11 @@ const index =()=> {
 							
 						</div>
 				</div>
+			</div>
+		</section>
+		<section className='section2'>
+			<div className='grid grid-cols-4 gap-5'>
+                {filteredArr.map((item:any)=> <SectionOneItem arr={item} arrId={ganreArr}/>)}
 			</div>
 		</section>
     </AppLayout>
