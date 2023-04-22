df185d34db5066c219e35d1ea00a50cb5025861e
Mirsaid25 <siddikovmirsaid01@gmail.com>
2023-04-23 00:20:37 +0500

    23.04

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,8 @@ import '@/styles/globals.css'
 import axios from 'axios'
 import type { AppProps } from 'next/app'
 import Link from 'next/link'
-import { Router, useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
+import {useRouter } from 'next/router'
+import React, { useEffect, useState } from 'react'
 
 export const APIkey = "d8c00e564262e291fb38f263b4c7128e"
 
@@ -36,7 +36,7 @@ export default function App({ Component, pageProps }: AppProps) {
                 .then(res => {
                     if(res.status === 200 || res.status === 201){
                         setSessionId(res.data.session_id);
-                        // return res.data.session_id
+
                         axios.get(`https://api.themoviedb.org/3/account?api_key=${APIkey}&session_id=${res.data.session_id}`)
                             .then(res=> {
                                 if(res.status === 200 || res.status === 201){
@@ -59,8 +59,16 @@ export default function App({ Component, pageProps }: AppProps) {
                         <Component {...pageProps} />
                     </SessionIdContext.Provider>
                 ) 
-                : 
-                <Link href={`https://www.themoviedb.org/authenticate/${token}?redirect_to=http://localhost:3000/`}><h1>fghjkl;lkjhgfcx</h1></Link> 
+                :
+                (
+                    <div className='w-full flex flex-col items-center justify-center h-screen'>
+                        <Link href={`https://www.themoviedb.org/authenticate/${token}?redirect_to=http://localhost:3000/`} className="">
+                            <div className="text-white py-[5%] px-[15%]  bg-[#3657CB] rounded-[10px]">Потвердить токен</div>
+                        </Link> 
+                        
+                    </div>
+                )
+                // 
         )
         : 
         (<Preloader/>)
