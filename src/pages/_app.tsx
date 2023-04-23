6b3ff9cd9029011b31b4a5919df4b1f152610735
Mirsaid25 <siddikovmirsaid01@gmail.com>
2023-04-23 10:32:12 +0500

    23.04

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import axios from 'axios'
 import type { AppProps } from 'next/app'
 import Link from 'next/link'
 import {useRouter } from 'next/router'
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export const APIkey = "d8c00e564262e291fb38f263b4c7128e"
 
@@ -62,10 +62,9 @@ export default function App({ Component, pageProps }: AppProps) {
                 :
                 (
                     <div className='w-full flex flex-col items-center justify-center h-screen'>
-                        <Link href={`https://www.themoviedb.org/authenticate/${token}?redirect_to=http://localhost:3000/`} className="">
-                            <div className="text-white py-[5%] px-[15%]  bg-[#3657CB] rounded-[10px]">Потвердить токен</div>
+                        <Link href={`https://www.themoviedb.org/authenticate/${token}?redirect_to=http://localhost:3000/`} className="py-[5%] px-[15%] bg-[#3657CB] rounded-[10px]">
+                            <div className="text-white ">Потвердить токен</div>
                         </Link> 
-                        
                     </div>
                 )
                 // 
